a1a13b2d8056c9014a7539c536440445dfedd635
1DAM <1DAM@2ASIR-238>
2015-06-01 11:48:17 +0200

    Añado el metodo de insertar coches

--- src/Taller.c
+++ src/Taller.c
@@ -25,7 +25,6 @@ int size(struct Coche taller[]) {
 	return sizeof(taller) / sizeof(taller[0]);
 }
 
-
 int buscar(struct Coche taller[], char matricula[]) {
 	int i = 0;
 	do {
@@ -37,20 +36,33 @@ int buscar(struct Coche taller[], char matricula[]) {
 }
 
 void ordenar(struct Coche taller[]) {
-    int i, j;
-    for (i = 0; i < size(taller); ++i) {
-        for (j = i + 1; j < size(taller); ++j) {
-            if (strcasecmp(taller[i].matricula, taller[j].matricula)==1) {
-                struct Coche aux = taller[i];
-                taller[i] = taller[j];
-                taller[j] = aux;
-            }
-        }
-    }
+	int i, j;
+	for (i = 0; i < size(taller); ++i) {
+		for (j = i + 1; j < size(taller); ++j) {
+			if (strcasecmp(taller[i].matricula, taller[j].matricula) == 1) {
+				struct Coche aux = taller[i];
+				taller[i] = taller[j];
+				taller[j] = aux;
+			}
+		}
+	}
 
 }
 
+int insertar(struct Coche coche, struct Coche taller[]) {
 
+	if (buscar(taller, coche.matricula) != -1) {
+		return 2;
+	} else {
+		int i = buscar(taller, NULL);
+		if (i != -1) {
+
+			taller[i] = coche;
+			return 0;
+		}
+	}
+return 1;
+}
 
 int main(void) {
 
